1932d24bc4e675df3337da57007d21d38304d237
Sean Hetzel <seanhetzel1@gmail.com>
2019-06-28 19:58:20 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def artist
+    @artist
+  end
+
+  def artist=(artist_name)
+    @artist = artist_name
+  end
+
+  def self.all
+    @@all
+  end
+end
